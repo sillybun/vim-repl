231bcc633c6d2eade5dd0a7425b9413a79fe37b1
sillybun <zhangytfudan@gmail.com>
2019-08-03 22:25:33 +0800

    remove deprecated file

--- autoload/setup.py
+++
@@ -1,9 +0,0 @@
-#build the modules
-
-from distutils.core import setup, Extension
-
-# xtra_link_args=["-stdlib=libc++", "-mmacosx-version-min=10.9"]
-
-setup(name='afpython', version='1.0',  \
-      ext_modules=[Extension('afpython', ['afpython.cpp'], extra_link_args=["-stdlib=libc++", "-mmacosx-version-min=10.9"])])
-
