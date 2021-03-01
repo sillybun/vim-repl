806c98ebd5e782f9be388e2fbece2755f67b0021
张翼腾 <zhangyiteng@zhangyitengdeMacBook-Pro-2.local>
2021-03-01 19:16:16 +0800

    bug fix

--- test/test_automerge.py
+++ test/test_automerge.py
@@ -28,3 +28,14 @@ a = "abc\
         def"
 
 print(a)
+
+output1 = """
+R1#show ip interface brief
+Interface                  IP-Address      OK? Method Status                Protocol
+FastEthernet0/0            15.0.15.1       YES manual up                    up
+FastEthernet0/1            10.0.12.1       YES manual up                    up
+FastEthernet0/2            10.0.13.1       YES manual up                    up
+FastEthernet0/3            unassigned      YES unset  up                    up
+Loopback0                  10.1.1.1        YES manual up                    up
+Loopback100                100.0.0.1       YES manual up                    up
+"""
